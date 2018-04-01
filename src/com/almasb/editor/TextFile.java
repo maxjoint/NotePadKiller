63506f4d68867a04a8d064da03feac5c82addf4e
Maxim Chubachuk <maximchubachuk@MacBook-Pro-6.local>
2018-04-01 19:03:00 +0300

    Initial commit

---
+++ src/com/almasb/editor/TextFile.java
@@ -0,0 +1,23 @@
+package com.almasb.editor;
+
+import java.util.List;
+import java.nio.file.Path;
+
+public class TextFile {
+    private final Path file;
+    private final List<String> content;
+
+    public TextFile(Path file, List<String> content) {
+        this.file = file;
+        this.content = content;
+    }
+
+    public Path getFile() {
+        return file;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+}
+
